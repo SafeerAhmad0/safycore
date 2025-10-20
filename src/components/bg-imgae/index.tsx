8f1585ad94efd4091721f36adfc171c044749ecf
Safeer Ahmad <safeer1073@gmail.com>
2025-10-20 21:43:54 +0500

    h

--- src/components/bg-imgae/index.tsx
+++ src/components/bg-imgae/index.tsx
@@ -1,10 +1,11 @@
-import { svgs } from "@/src/publicImport";
 import Image from "next/image";
+
+const bgImage = "/images/bg-image.svg";
 
 export const BackgroundImageComponent = () => {
     return (
         <Image
-            src={svgs.bgImage}
+            src={bgImage}
             alt="Background Image"
             fill
             objectFit="cover"
